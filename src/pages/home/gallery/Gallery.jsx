d8ac323466d7c3ed6c3dd5aafb576c5a299a56f8
SaniaAhmed1 <tasminsania77@gmail.com>
2023-05-17 23:02:14 -0700

    gallery done

---
+++ src/pages/home/gallery/Gallery.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className="h-96 w-1/2 mx-auto carousel carousel-vertical rounded-box mt-20">
+  <div className="carousel-item h-full">
+    <img src="https://img.freepik.com/free-photo/portrait-excited-cute-little-kid-hat_171337-7118.jpg?size=626&ext=jpg&ga=GA1.1.632902743.1676570136&semt=ais" />
+  </div> 
+  <div className="carousel-item h-full">
+    <img src="https://img.freepik.com/free-photo/positive-girl-grey-dress-showing-fingers-hands_7502-4310.jpg?size=626&ext=jpg&ga=GA1.1.632902743.1676570136&semt=ais" />
+  </div> 
+  <div className="carousel-item h-full">
+    <img className='w-full' src="https://img.freepik.com/free-photo/portrait-funny-little-boy_93675-133873.jpg?size=626&ext=jpg&ga=GA1.1.632902743.1676570136&semt=ais" />
+  </div> 
+  <div className="carousel-item h-full">
+    <img src="https://img.freepik.com/free-photo/happy-young-boy-sitting-skateboard-showing-thumb-up_171337-7161.jpg?size=626&ext=jpg&ga=GA1.2.632902743.1676570136&semt=ais" />
+  </div> 
+  <div className="carousel-item h-full">
+    <img src="https://img.freepik.com/free-photo/happy-little-girl-celebrating-her-birthday_23-2148984850.jpg?size=626&ext=jpg&ga=GA1.2.632902743.1676570136&semt=ais" />
+  </div> 
+  <div className="carousel-item h-full">
+    <img src="https://img.freepik.com/free-photo/portrait-smiling-little-kid-standing_171337-7107.jpg?size=626&ext=jpg&ga=GA1.2.632902743.1676570136&semt=ais" />
+  </div> 
+  <div className="carousel-item h-full">
+    <img className='w-full' src="https://img.freepik.com/free-photo/young-boy-with-fists-closed_23-2148414493.jpg?size=626&ext=jpg&ga=GA1.1.632902743.1676570136&semt=ais" />
+  </div>
+</div>
+    );
+};
+
+export default Gallery;
