253ad0434536eea6042b997d060b6842c21844b9
SaniaAhmed1 <tasminsania77@gmail.com>
2023-05-21 07:15:16 -0700

    done

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
         <div className = ' my-20 flex items-center justify-center gap-5'>
             <h2 className = 'text-5xl font-bold text-center'>oops! the car is lost</h2>
             <img className='w-72 h-72 rounded' src="https://img.freepik.com/free-photo/fun-gorilla-3d-illustration_183364-80089.jpg?size=626&ext=jpg&ga=GA1.1.632902743.1676570136&semt=ais"  />
+            <Link className='text-info text-lg' to={'/'}>Go to home</Link>
         </div>
     );
 };
--- src/pages/Mytoys/UpdateToy.jsx
+++ src/pages/Mytoys/UpdateToy.jsx
@@ -14,23 +14,16 @@ const UpdateToy = () => {
     const updateToy = (e) => {
         e.preventDefault() ;
         const form = e.target;
-        const toy_name = form.toy_name.value;
-        const sub_category = form.sub_category.value;
+        
         const price = form.price.value;
-        const rating = form.rating.value;
+        
         const quantity = form.quantity.value;
         const details = form.details.value;
-        const photo = form.photo.value;
+        
         const updatedtoy = {
-            seller_name: user?.displayName ,
-            seller_email: user?.email,
-            toy_name,
-            sub_category,
             price,
-            rating,
             quantity,
-            details,
-            photo
+            details
         }
         console.log(updatedtoy)
 
@@ -62,23 +55,13 @@ const UpdateToy = () => {
         <div className='p-4'>
             <h3 className='my-5 font-semibold text-center text-4xl'>Update a Toy</h3>
             <form onSubmit={updateToy} className='bg-sky-700 p-6 rounded-lg' >
-                <label className="label">
-                    <span className="label-text text-2xl text-white font-semibold">Your details</span>
-                </label>
-                <input type="text" defaultValue={user?.displayName} name='seller_name' placeholder="Your name here" className="input my-4 input-bordered input-primary ml-4 w-1/4 " />
-                <input type="email" defaultValue={user?.email} name='seller_email' placeholder="Your email here" className="input my-4 input-bordered input-primary ml-4 w-1/4 " />
-                <br></br>
+                
                 <label className="label">
                     <span className="label-text text-2xl text-white font-semibold">Toy Info</span>
                 </label>
-                <input type="text" name='toy_name' defaultValue={toy_name} placeholder="Toy Name Here" className="input my-4 input-bordered input-primary ml-4 w-1/4 " />
-                <input type="text" defaultValue={sub_category} name='sub_category' placeholder="Sub category" className="input my-4 input-bordered input-primary ml-4 w-1/3 " />
                 <input type="text" defaultValue={price} name='price' placeholder="price" className="input my-4 ml-4 input-bordered input-primary w-1/4 " />
-                <input type="text" defaultValue={rating} name='rating' placeholder="ratings" className="input my-4 input-bordered input-primary ml-4 w-1/6 " />
                 <input type="text" defaultValue={quantity} name='quantity' placeholder="Available Quantity" className="input my-4 ml-4 input-bordered input-primary w-1/4 " />
                 <input type="text" defaultValue={details} name='details' placeholder="detail description" className="input my-4 input-bordered input-primary ml-4 w-full " />
-                <input type="text" defaultValue={photo} name='photo' placeholder="photo url" className="input my-4 input-bordered input-primary ml-4 w-full " />
-
                 <div className='flex justify-center w-full'>
                     <input type="submit" className='w-1/2 btn ' value="Update" />
                 </div>
